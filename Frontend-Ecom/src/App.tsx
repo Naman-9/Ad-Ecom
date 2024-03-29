0a0d123256fc2e92380482f976c54ef039182221
Naman-9 <namancp7@gmail.com>
2024-03-29 16:39:03 +0530

    Protected Route Setup

--- Frontend-Ecom/src/App.tsx
+++ Frontend-Ecom/src/App.tsx
@@ -10,6 +10,7 @@ import { userExist, userNotExist } from './redux/reducer/useReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from './redux/api/userAPI';
 import { UserReducerInitialState } from './types/reducer-types';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   const disptach = useDispatch();
@@ -71,44 +72,56 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
 
           {/* Not Logged In Route */}
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/login"
+            element={
+              <ProtectedRoute isAuthenticated={user ? false : true}>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Logged in User Routes */}
-          <Route></Route>
-          <Route path="/shipping" element={<Shipping />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/orders/:id" element={<OrderDetails />} />
+          <Route element={<ProtectedRoute isAuthenticated={user ? true : false} />}>
+            <Route path="/shipping" element={<Shipping />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/orders/:id" element={<OrderDetails />} />
+          </Route>
 
           {/* ---------------Admin-------------------- */}
           {/* ---------------Admin-------------------- */}
           {/* ---------------Admin-------------------- */}
           {/* ---------------Admin-------------------- */}
 
-          {/* <Route
-  element={
-    <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-  }
-> */}
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/product" element={<Products />} />
-          <Route path="/admin/customer" element={<Customers />} />
-          <Route path="/admin/transaction" element={<Transaction />} />
-          {/* Charts */}
-          <Route path="/admin/chart/bar" element={<Barcharts />} />
-          <Route path="/admin/chart/pie" element={<Piecharts />} />
-          <Route path="/admin/chart/line" element={<Linecharts />} />
-          {/* Apps */}
-          <Route path="/admin/app/coupon" element={<Coupon />} />
-          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
-          <Route path="/admin/app/toss" element={<Toss />} />
+          <Route
+            element={
+              <ProtectedRoute
+                isAuthenticated={true}
+                adminOnly={true}
+                isAdmin={user?.role === 'admin' ? true : false}
+              />
+            }
+          >
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/product" element={<Products />} />
+            <Route path="/admin/customer" element={<Customers />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            {/* Charts */}
+            <Route path="/admin/chart/bar" element={<Barcharts />} />
+            <Route path="/admin/chart/pie" element={<Piecharts />} />
+            <Route path="/admin/chart/line" element={<Linecharts />} />
+            {/* Apps */}
+            <Route path="/admin/app/coupon" element={<Coupon />} />
+            <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+            <Route path="/admin/app/toss" element={<Toss />} />
 
-          {/* Management */}
-          <Route path="/admin/product/new" element={<NewProduct />} />
+            {/* Management */}
+            <Route path="/admin/product/new" element={<NewProduct />} />
 
-          <Route path="/admin/product/:id" element={<ProductManagement />} />
+            <Route path="/admin/product/:id" element={<ProductManagement />} />
 
-          <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
-
+            <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
+          </Route>
           {/* ---------------Admin-------------------- */}
           {/* ---------------Admin-------------------- */}
           {/* ---------------Admin-------------------- */}
